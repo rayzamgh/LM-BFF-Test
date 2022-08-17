8c6701222f39b1c88c8cf154ad3682e9252441aa
Rayza Mahendra <rayzaganteng@gmail.com>
2022-08-17 10:52:33 +0700

    new

--- src/processors.py
+++ src/processors.py
@@ -246,48 +246,6 @@ class Sst2Processor(DataProcessor):
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
         return examples
 
-
-class SmsaProcessor(DataProcessor):
-    """Processor for the SMSA dataset."""
-
-    def get_example_from_tensor_dict(self, tensor_dict):
-        """See base class."""
-        return InputExample(
-            tensor_dict["idx"].numpy(),
-            tensor_dict["sentence"].numpy().decode("utf-8"),
-            None,
-            str(tensor_dict["label"].numpy()),
-        )
-
-    def get_train_examples(self, data_dir):
-        """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train_preprocess.tsv")), "train")
-
-    def get_dev_examples(self, data_dir):
-        """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "valid_preprocess.tsv")), "dev")
-
-    def get_test_examples(self, data_dir):
-        """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test_preprocess.tsv")), "test")
-
-    def get_labels(self):
-        """See base class."""
-        return ["positive", "negative" ,"neutral"]
-
-    def _create_examples(self, lines, set_type):
-        """Creates examples for the training, dev and test sets."""
-        examples = []
-        text_index = 0
-        for (i, line) in enumerate(lines):
-            if i == 0:
-                continue
-            guid = "%s-%s" % (set_type, i)
-            text_a = line[text_index]
-            label = line[1]
-            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
-        return examples
-
 class StsbProcessor(DataProcessor):
     """Processor for the STS-B data set (GLUE version)."""
 
